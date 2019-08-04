ecab6de4802b4972c0e6520ea8946847fad029bb
vojtechrojicek <vojtech.rojicek@gmail.com>
2019-08-04 18:02:44 +0200

    Swagger documentation update

--- ProductsWebApi.Web/Controllers/ProductsController.cs
+++ ProductsWebApi.Web/Controllers/ProductsController.cs
@@ -9,10 +9,7 @@ namespace ProductsWebApi.Web.Controllers
 {
     /// <summary>
     /// Products controller.
-    /// </summary>
-    /// <response code="200">Ok.</response>
-    /// <response code="400">Incorrect request. Check response message for more details.</response>
-    /// <response code="500">Unexpected error occurred.</response>
+    /// </summary>    
     [ApiVersion("1.0")]
     [Route("api/v{version:apiVersion}/[controller]")]
     [ApiController]
@@ -35,6 +32,9 @@ namespace ProductsWebApi.Web.Controllers
         /// Returns all product.
         /// </summary>
         /// <returns>List of all products.</returns>
+        /// <response code="200">Ok.</response>
+        /// <response code="400">Incorrect request. Check response message for more details.</response>
+        /// <response code="500">Unexpected error occurred.</response>
         [HttpGet]
         public ActionResult<List<Product>> GetProducts()
         {
@@ -46,7 +46,10 @@ namespace ProductsWebApi.Web.Controllers
         /// </summary>
         /// <param name="id">Id of the product.</param>        
         /// <returns>Requested product.</returns>
+        /// <response code="200">Ok.</response>
+        /// <response code="400">Incorrect request. Check response message for more details.</response>
         /// <response code="404">If product with <paramref name="id"/> doesn't exist.</response>
+        /// <response code="500">Unexpected error occurred.</response>
         [HttpGet("{id}")]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public ActionResult<Product> GetProduct(Guid id)
@@ -60,7 +63,9 @@ namespace ProductsWebApi.Web.Controllers
         /// <param name="id">Id of the product.</param>
         /// <param name="product">Data for updating product.</param>
         /// <response code="200">Updated.</response>
+        /// <response code="400">Incorrect request. Check response message for more details.</response>
         /// <response code="404">If product with <paramref name="id"/> doesn't exist.</response>
+        /// <response code="500">Unexpected error occurred.</response>
         [HttpPut("{id}")]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public ActionResult UpdateDescription(Guid id, ProductUpdate product)
